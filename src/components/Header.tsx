805ecd4b96e1d31327f6951e97a8d21af449aef2
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-01-22 12:34:35 -0300

    style modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import { NewHabitForm } from './NewHabitForm';
 import logoImage from '../assets/logo.svg';
 import { Plus, X } from 'phosphor-react';
 
@@ -23,7 +24,11 @@ export function Header() {
             <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200">
               <X size={24} aria-label="Fechar"/>
             </Dialog.Close>
-            Abre o modal
+            <Dialog.Title className="text-3xl leading-tight font-extrabold">
+              Criar hábito
+            </Dialog.Title>
+
+            <NewHabitForm />
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>

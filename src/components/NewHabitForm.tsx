38130972e827d02775f7adfb0889c95ec64adf11
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-01-22 21:16:46 -0300

    create new habit

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
-import { Check } from 'phosphor-react';
-import { FormEvent, useState } from 'react';
 import * as Checkbox from '@radix-ui/react-checkbox';
+import { FormEvent, useState } from 'react';
+import { Check } from 'phosphor-react';
+import { api } from '../lib/axios';
 
 const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
@@ -8,8 +9,21 @@ export function NewHabitForm() {
   const [title, setTitle] = useState('');
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
-  function createNewHabit(event: FormEvent) {
+  async function createNewHabit(event: FormEvent) {
     event.preventDefault()
+
+    if(!title || weekDays.length === 0) {
+      return alert("Você não preencheu o(s) campo(s) título ou dia.")
+    }
+
+    await api.post('habits', {
+      title,
+      weekDays,
+    })
+
+    alert('Hábito criado com sucesso!')
+    setTitle('');
+    setWeekDays([]);
   }
 
   function handleToggleWeekDay(weekDay: number) {
@@ -35,6 +49,7 @@ export function NewHabitForm() {
         placeholder="Ex.: Exercício físico, Leitura, Beber água, etc."
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
+        value={title}
         onChange={e => setTitle(e.target.value)}
       />
 
@@ -49,6 +64,7 @@ export function NewHabitForm() {
               <Checkbox.Root 
                 key={weekDay} 
                 className="flex items-center gap-3 group"
+                checked={weekDays.includes(index)}
                 onCheckedChange={() => handleToggleWeekDay(index)}
               >
                 <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500">
